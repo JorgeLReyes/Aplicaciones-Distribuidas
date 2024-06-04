856050e0c3e4f35dd8eda452981af4d8d3c1ebf4
Jorge Reyes <jorgereyes.130101@gmail.com>
2024-06-04 09:42:09 -0600

    Delete Proyecto final directory

--- Proyecto final/Server GraphQL/src/index.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-const app = require("./server/server");
-
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
